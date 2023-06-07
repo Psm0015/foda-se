c98232d0921771e64c403ca8b1557ec4e2ed092b
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-06-07 14:44:20 -0300

    Tempo real e pequenas alterações

--- src/main/java/com/fodase/fodase/Controllers/JogoFD.java
+++ src/main/java/com/fodase/fodase/Controllers/JogoFD.java
@@ -1,6 +1,5 @@
 package com.fodase.fodase.Controllers;
 
-import java.io.Console;
 import java.sql.Timestamp;
 import java.util.Date;
 
@@ -10,6 +9,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,16 +29,18 @@ public class JogoFD {
     @Autowired
     private RodadaRepository rRepository;
 
-    @PostMapping
-    public ResponseEntity<?> Cadastrar(@RequestBody Rodada rodada, Authentication authentication) {
+    @PostMapping("/aposta/{aposta}")
+    public ResponseEntity<?> Cadastrar(@PathVariable Integer aposta, Authentication authentication) {
         try {
             Usuario user = (Usuario) authentication.getPrincipal();
+            Rodada rodada = new Rodada();
             rodada.setUsuario(user);
             Date dataAtual = new Date();
             rodada.setData(new Timestamp(dataAtual.getTime()));
-            rRepository.save(rodada);
+            rodada.setAposta(aposta);
+            Rodada rd = rRepository.save(rodada);
             messagingTemplate.convertAndSend("/topic/greetings", rodada);
-            return ResponseEntity.ok().body("Aposta Feita com Sucesso");
+            return ResponseEntity.ok().body(rd);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e);
         }
